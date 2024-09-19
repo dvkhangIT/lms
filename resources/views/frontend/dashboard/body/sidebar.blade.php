0074caf17c0f93d4c36d520c00d61141cee72d64
khangIT <dvkhang2100321@student.ctuet.edu.vn>
2024-09-19 10:45:55 +0700

    User Profile Design

--- resources/views/frontend/dashboard/body/sidebar.blade.php
+++ resources/views/frontend/dashboard/body/sidebar.blade.php
@@ -1,9 +1,9 @@
 <div class="off-canvas-menu dashboard-off-canvas-menu off--canvas-menu custom-scrollbar-styled pt-20px">
-  <div class="off-canvas-menu-close dashboard-menu-close icon-element icon-element-sm shadow-sm" data-toggle="tooltip"
+  <div class="shadow-sm off-canvas-menu-close dashboard-menu-close icon-element icon-element-sm" data-toggle="tooltip"
     data-placement="left" title="Close menu">
     <i class="la la-times"></i>
   </div><!-- end off-canvas-menu-close -->
-  <div class="logo-box px-4">
+  <div class="px-4 logo-box">
     <a href="index.html" class="logo"><img src="{{ asset("frontend/images/logo.png") }}" alt="logo"></a>
   </div>
   <ul class="generic-list-item off-canvas-menu-list off--canvas-menu-list pt-35px">
@@ -13,7 +13,7 @@
           <path
             d="M19 5v2h-4V5h4M9 5v6H5V5h4m10 8v6h-4v-6h4M9 17v2H5v-2h4M21 3h-8v6h8V3zM11 3H3v10h8V3zm10 8h-8v10h8V11zm-10 4H3v6h8v-6z" />
         </svg> Dashboard</a></li>
-    <li><a href="dashboard-profile.html"><svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px"
+    <li><a href="{{ route("user.profile") }}"><svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px"
           viewBox="0 0 24 24" width="18px">
           <path d="M0 0h24v24H0V0z" fill="none" />
           <path
@@ -45,7 +45,7 @@
           <path d="M0 0h24v24H0V0z" fill="none" />
           <path
             d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2zm6-6v-5c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.64 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2zm-2 1H8v-6c0-2.48 1.51-4.5 4-4.5s4 2.02 4 4.5v6z" />
-        </svg> Message <span class="badge badge-info ml-2 p-1">2</span></a></li>
+        </svg> Message <span class="p-1 ml-2 badge badge-info">2</span></a></li>
     <li><a href="dashboard-reviews.html"><svg class="mr-2" xmlns="http://www.w3.org/2000/svg" height="18px"
           viewBox="0 0 24 24" width="18px">
           <path d="M0 0h24v24H0V0z" fill="none" />
@@ -66,8 +66,8 @@
   </ul>
 </div><!-- end off-canvas-menu -->
 <div class="dashboard-content-wrap">
-  <div class="dashboard-menu-toggler btn theme-btn theme-btn-sm lh-28 theme-btn-transparent mb-4 ml-3">
-    <i class="la la-bars mr-1"></i> Dashboard Nav
+  <div class="mb-4 ml-3 dashboard-menu-toggler btn theme-btn theme-btn-sm lh-28 theme-btn-transparent">
+    <i class="mr-1 la la-bars"></i> Dashboard Nav
   </div>
   <div class="container-fluid">
     @yield("userDashboard")
--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,8 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+  Route::get('/user/profile', [UserController::class, 'UserProfile'])->name('user.profile');
+  Route::post('/user/profile/update', [UserController::class, 'UserProfileUpdate'])->name('user.profile.update');
 });
 require __DIR__ . '/auth.php';
 

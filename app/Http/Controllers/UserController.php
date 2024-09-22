eb165b2042250c03e4cb89fa2e510e1d4bcd9925
khangIT <dvkhang2100321@student.ctuet.edu.vn>
2024-09-22 19:27:07 +0700

    User Logout Option

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -41,4 +41,11 @@ class UserController extends Controller
     );
     return redirect()->back()->with($notification);
   }
+  public function UserLogout(Request $request)
+  {
+    Auth::guard('web')->logout();
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+    return redirect('/login');
+  }
 }

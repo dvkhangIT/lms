4fdb726fe8dcd4d10cf41a325b20023a81ceeef8
Khang Duong Van <khangduong.dev@gmail.com>
2025-01-05 17:18:08 +0700

    16 - Frontend Setup Course Details Page Part 3

--- resources/views/frontend/course/course_details.blade.php
+++ resources/views/frontend/course/course_details.blade.php
@@ -90,31 +90,10 @@
             <div class="p-4 rounded course-overview-card bg-gray">
               <h3 class="pb-3 fs-24 font-weight-semi-bold">What you'll learn?</h3>
               <ul class="generic-list-item overview-list-item">
-                <li><i class="mr-1 text-black la la-check"></i> Learn the core
-                  Java skills needed to
-                  apply for Java
-                  developer positions in just 14 hours.</li>
-                <li><i class="mr-1 text-black la la-check"></i> Be able to
-                  demonstrate your
-                  understanding of Java to
-                  future employers.</li>
-                <li><i class="mr-1 text-black la la-check"></i> Acquire essential
-                  java basics for
-                  transitioning to the
-                  Spring Framework, Java EE, Android development and more.</li>
-                <li><i class="mr-1 text-black la la-check"></i> Be able to sit for
-                  and pass the Oracle
-                  Java Certificate
-                  exam if you choose.</li>
-                <li><i class="mr-1 text-black la la-check"></i> Learn industry
-                  "best practices" in
-                  Java software
-                  development from a professional Java developer who has worked in
-                  the language for 18
-                  years.</li>
-                <li><i class="mr-1 text-black la la-check"></i> Obtain proficiency
-                  in Java 8 and Java
-                  11.</li>
+                @foreach ($goals as $goal)
+                  <li><i class="mr-1 text-black la la-check"></i>
+                    {{ $goal->goal_name }}</li>
+                @endforeach
               </ul>
             </div><!-- end course-overview-card -->
             <div class="p-4 rounded course-overview-card bg-gray">
@@ -126,9 +105,7 @@
             <div class="course-overview-card">
               <h3 class="pb-3 fs-24 font-weight-semi-bold">Requirements</h3>
               <ul class="generic-list-item generic-list-item-bullet fs-15">
-                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+                <li>{{ $course->prerequisites }}</li>
               </ul>
             </div><!-- end course-overview-card -->
             <div class="p-4 border rounded course-overview-card border-gray">
@@ -137,13 +114,17 @@
               <p class="pb-1 fs-15">Get your team access to Aduca's top 5,000+
                 courses</p>
               <div class="pb-3">
-                <img width="85" class="mr-3" src="images/sponsor-img.png"
+                <img width="85" class="mr-3"
+                  src="{{ asset("frontend/images/sponsor-img.png") }}"
                   alt="company logo">
-                <img width="80" class="mr-3" src="images/sponsor-img2.png"
+                <img width="80" class="mr-3"
+                  src="{{ asset("frontend/images/sponsor-img2.png") }}"
                   alt="company logo">
-                <img width="80" class="mr-3" src="images/sponsor-img3.png"
+                <img width="80" class="mr-3"
+                  src="{{ asset("frontend/images/sponsor-img3.png") }}"
                   alt="company logo">
-                <img width="70" class="mr-3" src="images/sponsor-img4.png"
+                <img width="70" class="mr-3"
+                  src="{{ asset("frontend/images/sponsor-img4.png") }}"
                   alt="company logo">
               </div>
               <a href="for-business.html" class="btn theme-btn theme-btn-sm">Try
@@ -152,60 +133,13 @@
             </div><!-- end course-overview-card -->
             <div class="course-overview-card">
               <h3 class="pb-3 fs-24 font-weight-semi-bold">Description</h3>
-              <p class="pb-2 fs-15">Lorem Ipsum has been the industry’s standard
-                dummy text ever since
-                the 1500s, when an
-                unknown printer took a galley of type and scrambled it to make a
-                type specimen book.
-                It has survived not
-                only five centuries, Lorem Ipsum has been the industry’s standard
-                dummy text ever
-                since the 1500s, when an
-                unknown printer took a galley of type and scrambled it to make a
-                type specimen book.
-              </p>
-              <p class="pb-2 fs-15">It has survived not only five centuries, but
-                also the leap into
-                electronic
-                typesetting, remaining essentially unchanged.Lorem Ipsum is simply
-                dummy text of the
-                printing and
-                typesetting industry. Lorem Ipsum has been the industry’s standard
-                dummy</p>
-              <p class="pb-1 fs-15">- Lorem ipsum dolor sit amet, consectetur.</p>
-              <p class="pb-1 fs-15">- Lorem ipsum dolor sit amet, consectetur.</p>
-              <p class="pb-1 fs-15">- Lorem ipsum dolor sit amet, consectetur.</p>
-              <p class="pt-3 pb-2 fs-15 lh-22"><strong
-                  class="text-black font-weight-semi-bold">Are
-                  you aiming to get
-                  your first Java Programming job but struggling to find out what
-                  skills employers
-                  want</strong> and which
-                course will give you those skills?</p>
-              <p class="pb-2 fs-15">This course is designed to give you the Java
-                skills you need to
-                get a job as a Java
-                developer. By the end of the course, you will understand Java
-                extremely well and be
-                able to build your own
-                Java apps and be productive as a software developer.</p>
+              <p class="pb-2 fs-15">{!! $course->description !!}</p>
               <div class="collapse" id="collapseMore">
-                <p class="pb-2 fs-15">The core java material you need to learn
-                  java development is
-                  covered in the first
-                  seven sections (around 14 hours in total). The Java Basics are
-                  covered in those
-                  sections. The rest of
-                  the course covers intermediate, advanced, and optional material
-                  you do not
-                  technically need to go
-                  through.</p>
+                <p class="pb-2 fs-15">{{ $course->prerequisites }}</p>
                 <h4 class="py-2 fs-20 font-weight-semi-bold">Who this course is
                   for:</h4>
                 <ul class="generic-list-item generic-list-item-bullet fs-15">
-                  <li>Anyone who wants to become a computer programmer</li>
-                  <li>Anyone who wants to become a computer programmer</li>
-                  <li>Anyone who wants to become a computer programmer</li>
+                  <p class="pb-2 fs-15">{{ $course->prerequisites }}</p>
                 </ul>
               </div>
               <a class="collapse-btn collapse--btn fs-15" data-toggle="collapse"
@@ -1418,7 +1352,6 @@
       </div><!-- end modal-content-->
     </div><!-- end modal-dialog -->
   </div><!-- end modal -->
-
   <!-- Modal -->
   <div class="modal fade modal-container" id="previewModal" tabindex="-1"
     role="dialog" aria-labelledby="previewModalTitle" aria-hidden="true">
@@ -1456,7 +1389,6 @@
       </div><!-- end modal-content -->
     </div><!-- end modal-dialog -->
   </div><!-- end modal -->
-
   <!-- Modal -->
   <div class="modal fade modal-container" id="reportModal" tabindex="-1"
     role="dialog" aria-labelledby="reportModalTitle" aria-hidden="true">

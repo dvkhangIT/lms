8bfd04013c3f003dcd0a84a88bc79cb7ea4700d2
Khang Duong Van <khangduong.dev@gmail.com>
2025-01-06 07:43:55 +0700

    16 - Frontend Setup Course Details Page Part 4

--- resources/views/frontend/course/course_details.blade.php
+++ resources/views/frontend/course/course_details.blade.php
@@ -151,188 +151,82 @@
                     class="ml-1 la la-angle-up fs-14"></i></span>
               </a>
             </div><!-- end course-overview-card -->
+            @php
+              $lecture = App\Models\CourseLecture::where(
+                  "course_id",
+                  $course->id,
+              )->get();
+            @endphp
             <div class="course-overview-card">
               <div
                 class="pb-4 curriculum-header d-flex align-items-center justify-content-between">
                 <h3 class="fs-24 font-weight-semi-bold">Course content</h3>
                 <div class="curriculum-duration fs-15">
                   <span class="mr-2 curriculum-total__text"><strong
-                      class="text-black font-weight-semi-bold">Total:</strong> 17
+                      class="text-black font-weight-semi-bold">Total:</strong>
+                    {{ count($lecture) }}
                     lectures</span>
                   <span class="curriculum-total__hours"><strong
                       class="text-black font-weight-semi-bold">Total
-                      hours:</strong> 02:35:47</span>
+                      hours:</strong> {{ $course->duration }}</span>
                 </div>
               </div>
+              @php
+                $section = App\Models\CourseSection::where(
+                    "course_id",
+                    $course->id,
+                )
+                    ->orderBy("id", "ASC")
+                    ->get();
+              @endphp
               <div class="curriculum-content">
                 <div id="accordion" class="generic-accordion">
-                  <div class="card">
-                    <div class="card-header" id="headingOne">
-                      <button
-                        class="btn btn-link d-flex align-items-center justify-content-between"
-                        data-toggle="collapse" data-target="#collapseOne"
-                        aria-expanded="true" aria-controls="collapseOne">
-                        <i class="la la-plus"></i>
-                        <i class="la la-minus"></i>
-                        Course introduction
-                        <span class="fs-15 text-gray font-weight-medium">6
-                          lectures</span>
-                      </button>
-                    </div><!-- end card-header -->
-                    <div id="collapseOne" class="collapse show"
-                      aria-labelledby="headingOne" data-parent="#accordion">
-                      <div class="card-body">
-                        <ul class="generic-list-item">
-                          <li>
-                            <a href="#"
-                              class="d-flex align-items-center justify-content-between text-color"
-                              data-toggle="modal" data-target="#previewModal">
-                              <span>
-                                <i class="mr-1 la la-play-circle"></i>
-                                Introductory words
-                                <span class="ml-2 ribbon fs-13">Preview</span>
-                              </span>
-                              <span>02:27</span>
-                            </a>
-                          </li>
-                          <li>
-                            <div
-                              class="d-flex align-items-center justify-content-between">
-                              <span>
-                                <i class="mr-1 la la-play-circle"></i>
-                                Remaster in Progress
-                              </span>
-                              <span>03:09</span>
-                            </div>
-                          </li>
-                          <li>
-                            <div
-                              class="d-flex align-items-center justify-content-between">
-                              <span>
-                                <i class="mr-1 la la-play-circle"></i>
-                                Video Quality
-                              </span>
-                              <span>01:16</span>
-                            </div>
-                          </li>
-                          <li>
-                            <div
-                              class="d-flex align-items-center justify-content-between">
-                              <span>
-                                <i class="mr-1 la la-play-circle"></i>
-                                Important Tip - Source Code
-                              </span>
-                              <span>02:07</span>
-                            </div>
-                          </li>
-                        </ul>
-                      </div><!-- end card-body -->
-                    </div><!-- end collapse -->
-                  </div><!-- end card -->
-                  <div class="card">
-                    <div class="card-header" id="headingTwo">
-                      <button
-                        class="btn btn-link collapsed d-flex align-items-center justify-content-between"
-                        data-toggle="collapse" data-target="#collapseTwo"
-                        aria-expanded="false" aria-controls="collapseTwo">
-                        <i class="la la-plus"></i>
-                        <i class="la la-minus"></i>
-                        Software tools setup
-                        <span class="fs-15 text-gray font-weight-medium">6
-                          lectures</span>
-                      </button>
-                    </div>
-                    <div id="collapseTwo" class="collapse"
-                      aria-labelledby="headingTwo" data-parent="#accordion">
-                      <div class="card-body">
-                        <ul class="generic-list-item">
-                          <li>
-                            <div
-                              class="d-flex align-items-center justify-content-between">
-                              <span>
-                                <i class="mr-1 la la-play-circle"></i>
-                                Biggest Tip to Succeed as a Java Programmer
-                              </span>
-                              <span>02:27</span>
-                            </div>
-                          </li>
-                          <li>
-                            <div
-                              class="d-flex align-items-center justify-content-between">
-                              <span>
-                                <i class="mr-1 la la-file"></i>
-                                ** IMPORTANT ** - Configuring IntelliJ IDEA
-                              </span>
-                              <span>00:16</span>
-                            </div>
-                          </li>
-                          <li>
-                            <div
-                              class="d-flex align-items-center justify-content-between">
-                              <span>
-                                <i class="mr-1 la la-play-circle"></i>
-                                Video Quality
-                              </span>
-                              <span>01:16</span>
-                            </div>
-                          </li>
-                          <li>
-                            <div
-                              class="d-flex align-items-center justify-content-between">
-                              <span>
-                                <i class="mr-1 la la-play-circle"></i>
-                                Important Tip - Source Code
-                              </span>
-                              <span>02:07</span>
-                            </div>
-                          </li>
-                          <li>
-                            <div
-                              class="d-flex align-items-center justify-content-between">
-                              <span>
-                                <i class="mr-1 la la-code"></i>
-                                Interface
-                              </span>
-                              <span>1 question</span>
-                            </div>
-                          </li>
-                        </ul>
-                      </div><!-- end card-body -->
-                    </div>
-                  </div><!-- end card -->
-                  <div class="card">
-                    <div class="card-header" id="headingThree">
-                      <button
-                        class="btn btn-link collapsed d-flex align-items-center justify-content-between"
-                        data-toggle="collapse" data-target="#collapseThree"
-                        aria-expanded="false" aria-controls="collapseThree">
-                        <i class="la la-plus"></i>
-                        <i class="la la-minus"></i>
-                        Conclusion
-                        <span class="fs-15 text-gray font-weight-medium">1
-                          lectures</span>
-                      </button>
-                    </div>
-                    <div id="collapseThree" class="collapse"
-                      aria-labelledby="headingThree" data-parent="#accordion">
-                      <div class="card-body">
-                        <ul class="generic-list-item">
-                          <li>
-                            <a href="#"
-                              class="d-flex align-items-center justify-content-between text-color"
-                              data-toggle="modal" data-target="#previewModal">
-                              <span>
-                                <i class="mr-1 la la-play-circle"></i>
-                                Conclusion
-                                <span class="ml-2 ribbon fs-13">Watch</span>
-                              </span>
-                              <span>02:27</span>
-                            </a>
-                          </li>
-                        </ul>
-                      </div><!-- end card-body -->
-                    </div><!-- end collapse -->
-                  </div><!-- end card -->
+                  @foreach ($section as $sec)
+                    @php
+                      $lecture = App\Models\CourseLecture::where(
+                          "section_id",
+                          $sec->id,
+                      )->get();
+                    @endphp
+                    <div class="card">
+                      <div class="card-header" id="heading{{ $sec->id }}">
+                        <button
+                          class="btn btn-link d-flex align-items-center justify-content-between"
+                          data-toggle="collapse"
+                          data-target="#collapse{{ $sec->id }}"
+                          aria-expanded="true"
+                          aria-controls="collapse{{ $sec->id }}">
+                          <i class="la la-plus"></i>
+                          <i class="la la-minus"></i>
+                          {{ $sec->section_title }}
+                          <span
+                            class="fs-15 text-gray font-weight-medium">{{ count($lecture) }}
+                            lectures</span>
+                        </button>
+                      </div><!-- end card-header -->
+                      <div id="collapse{{ $sec->id }}"
+                        class="collapse show"
+                        aria-labelledby="heading{{ $sec->id }}"
+                        data-parent="#accordion">
+                        <div class="card-body">
+                          <ul class="generic-list-item">
+                            @foreach ($lecture as $lect)
+                              <li>
+                                <div
+                                  class="d-flex align-items-center justify-content-between">
+                                  <span>
+                                    <i class="mr-1 la la-play-circle"></i>
+                                    {{ $lect->lecture_title }}
+                                  </span>
+                                  <span>03:09</span>
+                                </div>
+                              </li>
+                            @endforeach
+                          </ul>
+                        </div><!-- end card-body -->
+                      </div><!-- end collapse -->
+                    </div><!-- end card -->
+                  @endforeach
                 </div><!-- end generic-accordion -->
               </div><!-- end curriculum-content -->
             </div><!-- end course-overview-card -->

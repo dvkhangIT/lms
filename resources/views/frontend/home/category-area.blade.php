09209aaf6677a39e8b7949571a2471b81dd7e697
Khang Duong Van <khangduong.dev@gmail.com>
2025-01-09 12:49:00 +0700

    Home Category Wise Course

--- resources/views/frontend/home/category-area.blade.php
+++ resources/views/frontend/home/category-area.blade.php
@@ -15,7 +15,8 @@
       </div>
       <div class="col-lg-3">
         <div class="text-right category-btn-box">
-          <a href="categories.html" class="btn theme-btn">All Categories <i class="ml-1 la la-arrow-right icon"></i></a>
+          <a href="categories.html" class="btn theme-btn">All Categories <i
+              class="ml-1 la la-arrow-right icon"></i></a>
         </div>
       </div>
     </div>
@@ -27,13 +28,15 @@
           @endphp
           <div class="col-lg-4 responsive-column-half">
             <div class="category-item">
-              <img class="cat__img lazy" src="{{ asset($cat->image) }}" data-src="{{ asset($cat->image) }}"
-                alt="course image">
+              <img class="cat__img lazy" src="{{ asset($cat->image) }}"
+                data-src="{{ asset($cat->image) }}" alt="course image">
               <div class="category-content">
                 <div class="category-inner">
-                  <h3 class="cat__title"><a href="#">{{ $cat->category_name }}</a></h3>
+                  <h3 class="cat__title"><a
+                      href="#">{{ $cat->category_name }}</a></h3>
                   <p class="cat__meta">{{ count($course) }} courses</p>
-                  <a href="#" class="btn theme-btn theme-btn-sm theme-btn-white">Explore<i
+                  <a href="{{ url("category/" . $cat->id . "/" . $cat->category_slug) }}"
+                    class="btn theme-btn theme-btn-sm theme-btn-white">Explore<i
                       class="ml-1 la la-arrow-right icon"></i></a>
                 </div>
               </div><!-- end category-content -->

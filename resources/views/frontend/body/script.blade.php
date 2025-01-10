94ad4c320c122074183cfb27bcabcc96b5000e54
Khang Duong Van <khangduong.dev@gmail.com>
2025-01-10 20:50:51 +0700

    20 - Wishlist Read Course With Ajax Part 2

--- resources/views/frontend/body/script.blade.php
+++ resources/views/frontend/body/script.blade.php
@@ -48,9 +48,38 @@
       url: "/get-wishlist-course/",
       dataType: "json",
       success: function(response) {
-
+        var row = ''
+        $.each(response.wishlist, function(key, value) {
+          row += `
+           <div class="col-lg-4 responsive-column-half">
+        <div class="card card-item">
+          <div class="card-image">
+            <a href="course-details.html" class="d-block">
+              <img class="card-img-top" src="/${value.course.course_image}" alt="Card image cap">
+            </a>
+          </div><!-- end card-image -->
+          <div class="card-body">
+            <h6 class="ribbon ribbon-blue-bg fs-14 mb-3">${value.course.label}</h6>
+            <h5 class="card-title"><a href="course-details.html">${value.course.course_name}</a></h5>
+            <div class="d-flex justify-content-between align-items-center">
+              ${value.course.discount_price ==null 
+              ? `<p class="card-price text-black font-weight-bold">$${value.course.selling_price}</p>`
+              : `<p class="card-price text-black font-weight-bold">$${value.course.discount_price} <span
+                  class="before-price font-weight-medium">$${value.course.selling_price}</span></p>`}
+              <div class="icon-element icon-element-sm shadow-sm cursor-pointer"
+                data-toggle="tooltip" data-placement="top" title=""
+                data-original-title="Remove from Wishlist"><i
+                  class="la la-heart"></i></div>
+            </div>
+          </div><!-- end card-body -->
+        </div><!-- end card -->
+      </div>
+      `
+        });
+        $('#wishlist').html(row)
       }
     });
   }
+  wishlist()
 </script>
 {{-- end load wishlist data --}}

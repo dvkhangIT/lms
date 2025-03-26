1186904f02dc8ce87f833e4eef64730fb046b182
dvkhangIT <khangduong.dev@gmail.com>
2025-03-26 13:53:56 +0700

    44 - Course Review System Part 3

--- app/Http/Controllers/Backend/ReviewController.php
+++ app/Http/Controllers/Backend/ReviewController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Review;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReviewController extends Controller
 {
   public function StoreReview(Request $request)
   {
-    dd($request->all());
+    $course = $request->course_id;
+    $instructor = $request->instructor_id;
+    $request->validate([
+      'comment' => 'required'
+    ]);
+    Review::insert([
+      'course_id' => $course,
+      'user_id' => Auth::id(),
+      'comment' => $request->comment,
+      'rating' => $request->rate,
+      'instructor_id' => $request->instructor_id,
+      'created_at' => Carbon::now(),
+    ]);
+    $notification = array(
+      'message' => 'Review Will Approve By Admin',
+      'alert-type' => 'success'
+    );
+    return redirect()->back()->with($notification);
   }
 }

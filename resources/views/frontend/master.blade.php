055fc6e3cf9fbf610a9467dacfbb7ddb0cad4d08
dvkhangIT <khangduong.dev@gmail.com>
2025-04-05 18:58:38 +0700

    52 - Update Site title

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -6,7 +6,7 @@
     <meta name="author" content="TechyDevs">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>LMS</title>
+    <title>@yield('title')</title>
     <!-- Google fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link
